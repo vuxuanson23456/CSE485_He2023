5683997dcf1be3826afe933f997a8e0e9f25cd7f
vuxuanson23456 <140165675+vuxuanson23456@users.noreply.github.com>
2023-07-22 08:55:43 +0700

    Add files via upload

---
+++ TH1A1.php
@@ -0,0 +1,24 @@
+//Bài 1
+ hàm tính tổng
+<?php
+$prime_numbers = array(2, 5, 6, 9, 2, 5, 6, 12 ,5);
+echo array_sum($prime_numbers);
+?>
+//hàm tính tích
+<?php
+$prime_numbers = array(2, 5, 6, 9, 2, 5, 6, 12 ,5);
+echo array_product($prime_numbers);
+?>
+//hàm tính hiệu
+<?php
+$prime_numbers = array(2, 5, 6, 9, 2, 5, 6, 12 ,5);
+echo array_difference($prime_numbers);
+?>
+//thương các phần tử
+<?php
+$prime_numbers = array(2, 5, 6, 9, 2, 5, 6, 12 ,5);
+echo array_quotient($prime_numbers);
+?>
+
+
+// Bài 2
